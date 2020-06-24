f2b2d204777257209b7679e043829172b0f8fd02
Devinhodge <d.hodge8891@yahoo.com>
2020-06-24 11:50:46 -0500

    spec 2-2 is working

--- masterMind/main.js
+++ masterMind/main.js
@@ -1,12 +1,10 @@
 'use strict'
 //assert
 const assert = require('assert');
-
 //readline library is used to read input from the user
 const readline = require('readline');
-
 //setting up the realine settings
-const rl = realine.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
@@ -16,7 +14,19 @@ let solution = 'abcd'; //try to make the program generate a random solution
 
 
 
+let generateHint = function(guess){
+  let solutionArray = solution.split('');
+  let guessArray = guess.split('');
+  let correctLetterLocations = 0;
+  for(let i = 0; i < solutionArray.length; i++) {
+    if (solutionArray[i] === guessArray[i]){
+      correctLetterLocations++;
+      solutionArray[i] = null;
+    }
 
+  }
+  return correctLetterLocations;
+}
 
 
 
@@ -25,6 +35,10 @@ let solution = 'abcd'; //try to make the program generate a random solution
 let askForGuess = function(){
   rl.question("Guess: ", function(guess) {
     let hint = generateHint(guess);
+    console.log(hint);
+    if(guess === solution) {
+      console.log("You guessed it");
+    }
     // .. some more stuff
     let gameOver; // figure out how to set this
 
