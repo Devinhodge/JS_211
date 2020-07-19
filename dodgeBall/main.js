c2069c174d41c3c9368fad60c04c775bd15c1446
Devinhodge <d.hodge8891@yahoo.com>
2020-07-19 14:18:53 -0500

    trying to fix classes

--- dodgeBall/main.js
+++ dodgeBall/main.js
@@ -1,5 +1,5 @@
 'use strict'
-
+/*
 // list of players to pick for a game of Dodge Ball
 const arrOfPeople = [
     {
@@ -220,4 +220,102 @@ if (typeof discribe === 'function'){
       joinBlue('Adverage Jo');
     });
   });
+}
+*/
+
+const arrOfPeople = [
+  {
+    id: 2,
+    name: "Charles Young",
+    age: 55,
+    skillSet: "welding",
+    placeBorn: "Omaha, Nebraska"
+  },
+  {
+    id: 3,
+    name: "Judy Twilight",
+    age: 35,
+    skillSet: "fishing",
+    placeBorn: "Louisville, Kentucky"
+  },
+  {
+    id: 4,
+    name: "Cynthia Doolittle",
+    age: 20,
+    skillSet: "tic tac toe",
+    placeBorn: "Pawnee, Texas"
+  },
+  {
+    id: 5,
+    name: "John Willouby",
+    age: 28,
+    skillSet: "pipe fitting",
+    placeBorn: "New York, New York"
+  },
+  {
+    id: 6,
+    name: "Stan Honest",
+    age: 20,
+    skillSet: "boom-a-rang throwing",
+    placeBorn: "Perth, Australia"
+  },
+  {
+    id: 7,
+    name: "Mia Watu",
+    age: 17,
+    skillSet: "acrobatics",
+    placeBorn: "Los Angeles, California"
+  },
+  {
+    id: 8,
+    name: "Walter Cole",
+    age: 32,
+    skillSet: "jump rope",
+    placeBorn: "New Orleans, Louisiana"
+  },
+]
+
+const listOfPlayers = []
+const blueTeam = []
+const redTeam = []
+
+class player {
+  constructor(person, canThrowBall, canDodgeBall, hasPaid, isHealthy, yearsExperience){
+    this.person= [person],
+    this.canThrowBall = canThrowBall, 
+    this.canDodgeBall = canDodgeBall,
+    this.hasPaid = hasPaid, 
+    this.isHealthy = isHealthy,
+    this.yearsExperience =yearsExperience,
+    this.players = []
+  }
+  addPlayer(player){
+    this.players.push(player);
+  }
+}
+class Teammate {
+  constructor(person){
+    this.person = person;
+    this.players = [];
+  }
+  joinTeam(member){
+    this.members.push(member);
+  }
+}
+
+const listPeopleChoices = () => {
+  const listElement = document.getElementById('people')
+  arrOfPeople.map(person => {
+    const li = document.createElement("li")
+    const button = document.createElement("button")
+    button.innerHTML = "Make Player"
+    button.addEventListener('click', function() {makePlayer(person.id)} )
+    li.appendChild(button)
+    li.appendChild(document.createTextNode(person.name + " - " + person.skillSet))
+    listElement.append(li)
+  })
+}
+
+const makePlayer = (id) => {
+  console.log(`li ${id} was clicked!`)
 }
